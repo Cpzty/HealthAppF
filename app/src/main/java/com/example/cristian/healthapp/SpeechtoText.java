70e32cd2d3e721d5d8f13583e3d89db257701718
Cpzty <Cristianpzty@gmail.com>
2017-04-25 19:23:07 -0600

    Initial commit

---
+++ app/src/main/java/com/example/cristian/healthapp/SpeechtoText.java
@@ -0,0 +1,79 @@
+package com.example.cristian.healthapp;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.text.DecimalFormat;
+import android.os.AsyncTask;
+import android.text.method.ScrollingMovementMethod;
+
+
+import com.ibm.watson.developer_cloud.dialog.v1.model.Conversation;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class SpeechtoText extends AppCompatActivity {
+
+    private  TextView resultTEXT;
+    private TextView watsonResp;
+    private String text;
+    private Conversation service;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_speechto_text);
+        // Set up the Watson Q&A Service
+//                service = new Conversation();
+//        service.setClientId("username", "password");
+//        service.set(QuestionAndAnswerDataset.TRAVEL);
+        resultTEXT =  (TextView)findViewById(R.id.TVresult);
+    }
+
+    public void onButtonClick(View v){
+
+        if(v.getId() == R.id.imageButton){
+
+
+            promptSpeechInput();
+
+        }
+
+    }
+
+    public void promptSpeechInput(){
+
+        Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, Locale.getDefault());
+        i.putExtra(RecognizerIntent.EXTRA_PROMPT, "Di algo");
+
+        try {
+            startActivityForResult(i, 100);
+        }catch (ActivityNotFoundException a){
+            Toast.makeText(SpeechtoText.this,"No es posible en este dispositivo!",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void onActivityResult(int request_code, int result_code, Intent i){
+        super.onActivityResult(request_code,result_code,i);
+
+        switch (request_code){
+            case 100: if(result_code== RESULT_OK && i != null){
+                ArrayList<String>result = i.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                resultTEXT.setText(result.get(0));
+
+            }
+                break;
+        }
+    }
+
+
+
+}
